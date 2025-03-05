17b0daa585acfb54e023b10875ffcf7a1cecc24f
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2025-03-05 18:11:31 +0530

    update enrollments

--- router/enrollmentRouts.js
+++ router/enrollmentRouts.js
@@ -1,12 +1,9 @@
 import express from "express";
-import { enroll, getEnrollments, generateOrderId  } from "../controllers/enrollmentsController.js";
+import { enroll, getUserEnrollments, generateOrderId } from "../controllers/enrollmentsController.js";
 
 const router = express.Router();
 
 router.post("/enroll", enroll);
-router.get("/orderId", generateOrderId); // Endpoint to generate order ID
-router.get("/enrollments", getEnrollments); // Endpoint to get all enrollments
-
-
+router.get("/enrolled-courses/:userId", getUserEnrollments); 
 
 export default router;
